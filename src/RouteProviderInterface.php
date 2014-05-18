e0add9f370313417c35c38743f8509beac1444f6
Sean Mumford <sean.mumford@gmail.com>
2014-05-18 23:00:08 +0000

    adding docblocks

--- src/RouteProviderInterface.php
+++ src/RouteProviderInterface.php
@@ -5,7 +5,16 @@ namespace Thepsion5\Menuizer;
 
 interface RouteProviderInterface
 {
+    /**
+     * @param string $name The name of the route
+     * @return bool        True if the route exists, false otherwise
+     */
     public function namedRouteExists($name);
 
+    /**
+     * @param string $name   The name of the route
+     * @param array  $params Any parameters required as part of the route
+     * @return string        The provided route's url, or null if the route
+     */
     public function getNamedRoute($name, array $params = array());
 } 
